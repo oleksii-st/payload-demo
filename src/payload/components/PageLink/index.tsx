1de2473510f55a5c1af3350934a5de639079030f
Oleksii S <stokrockyioleksii@gmail.com>
2025-03-16 12:53:34 +0200

    fix: page link

--- src/payload/components/PageLink/index.tsx
+++ src/payload/components/PageLink/index.tsx
@@ -7,11 +7,22 @@ const PageLink = () => {
   const [fields] = useAllFormFields();
   const getPath = (): string => {
     if (!('breadcrumbs' in fields)) {
-      return `/blog/${fields.slug}`;
+      const slug = fields.slug.value;
+
+      if (!slug) {
+        return '';
+      }
+
+      return `/blog/${slug}`;
     }
 
     const breadcrumbsLength = fields.breadcrumbs.value as number;
     const pagePath = fields[`breadcrumbs.${breadcrumbsLength - 1}.url`]?.value;
+
+    if (!pagePath) {
+      return '';
+    }
+
     const path = pagePath === '/home' ? '/' : pagePath;
 
     return path as string;
@@ -19,6 +30,10 @@ const PageLink = () => {
 
   const path = getPath();
   const url = `${process.env.NEXT_PUBLIC_SITEMAP_URL}${path}`;
+
+  if (!path) {
+    return null;
+  }
 
   return (
     <div className="field-type text" style={{ marginTop: '16px' }}>
